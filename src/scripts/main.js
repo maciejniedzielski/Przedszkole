29525b82826a91beccb4ae1ca6d50ecde26fab3a
Introoz <niedzielamac@gmail.com>
2017-11-07 13:10:59 +0100

    Dodanie plików strony i ich edycja

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  $('a[href*="#"]:not([href="#"])').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top-70
+        }, 600);
+        window.location.hash = this.hash;
+    }
+  });
+
+
+});
